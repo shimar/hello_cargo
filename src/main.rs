059b5b455238ac0568b55958492d6a10af809bfe
Ryohei Shima <shima01dev@gmail.com>
2023-02-16 00:16:54 +0900

    Like a game.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
+
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    println!("The secret number is: {}", secret_number);
+
     println!("Please input your guess.");
 
     let mut guess = String::new();
@@ -9,5 +17,14 @@ fn main() {
         .read_line(&mut guess)
         .expect("Fail to read line");
 
+    let guess: u32 = guess.trim().parse()
+        .expect("Please type a number");
+
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
